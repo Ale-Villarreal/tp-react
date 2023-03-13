f01a5ae3f193509f7283b25c10d17141645d0137
Alejandro Villarreal <m.ale.villarreal.0803@gmail.com>
2023-03-12 23:19:34 -0300

    por fin pude agregar las cards

--- tp-react2/src/components/footer/Footer.jsx
+++ tp-react2/src/components/footer/Footer.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+
+    <Container fluid>
+      <Row className='bg-dark p-5 text-white'>
+        <Col>Empresa</Col>
+        <Col>Rik</Col>
+        <Col>Morty</Col>
+        <Col>Otras</Col>
+      </Row>
+    </Container>
+   
   )
 }
 
